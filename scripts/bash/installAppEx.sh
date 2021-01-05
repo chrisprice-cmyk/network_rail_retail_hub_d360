a1fb0cd537fa05c3c605c8fba1966dd3a12e3a6d
kakermanis <kakermanis@salesforce.com>
2021-01-05 13:56:24 +1100

    removed old scripts and simplified complex ones

--- scripts/bash/installAppEx.sh
+++
@@ -1,7 +0,0 @@
-echo
-echo '**************************************'
-echo 'Installing AppExchange Package '$1'...'
-echo '**************************************'
-echo
-
-sfdx force:package:install --package $1 -w 1000 -u $2
--- scripts/bash/installDependencies.sh
+++
@@ -1,8 +0,0 @@
-echo
-echo '**************************************'
-echo 'Installing any listed dependencies...'
-echo '**************************************'
-echo
-
-# Install any dependencies
-sfdx texei:package:dependencies:install -u $@
