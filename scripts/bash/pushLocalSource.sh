f672d864a149b4737004fc69d73315a9377baaf4
kakermanis <kakermanis@salesforce.com>
2021-01-05 13:53:25 +1100

    removed old scripts

--- scripts/bash/pushLocalSource.sh
+++
@@ -1,8 +0,0 @@
-echo
-echo '********************************************************'
-echo 'Pushing local code/config into the empty Scratch org...'
-echo '********************************************************'
-echo
-
-# Push local source
-sfdx force:source:push -u $@
--- scripts/bash/updateUserName.sh
+++
@@ -1,6 +0,0 @@
-echo
-echo '********************************************'
-echo 'Updating username to: '$2
-echo '********************************************'
-
-sfdx force:data:record:update -u $1 -s User -w "FirstName='User' LastName='User'" -v "Username='${2}'"
