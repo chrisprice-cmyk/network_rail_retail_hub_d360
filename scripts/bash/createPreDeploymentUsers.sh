adef4cabcc6ffc566399f3aea258a98a9be02f3c
kakermanis <kakermanis@salesforce.com>
2021-03-02 12:19:24 +1100

    shell comment

--- scripts/bash/createPreDeploymentUsers.sh
+++ scripts/bash/createPreDeploymentUsers.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+echo
+echo '**********************************'
+echo 'Creating Pre Deployment Users...'
+echo '**********************************'
+echo
 
 #sfdx force:apex:execute -f scripts/apex/createUser.apex
