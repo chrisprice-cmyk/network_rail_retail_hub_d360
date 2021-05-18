3ee40433e728c1d3a09b0baef45cf36c8d8157f3
Kevin Akermanis <kakermanis@salesforce.com>
2021-05-19 08:01:47 +1000

    community creation loop

--- scripts/bash/createCommunity.sh
+++ scripts/bash/createCommunity.sh
@@ -3,6 +3,7 @@ communityName=$1
 urlPrefix=$2
 templateName=$3
 desc=$4
+communityCreated=false
 
 sfdx force:data:record:get -s Network -w "Name='$communityName'" > testingCommunityExists.txt
 
@@ -12,8 +13,17 @@ else
   echo 'The '$communityName' does not exist so I will create it...'
   sfdx force:community:create --name "$communityName" --urlpathprefix $urlPrefix --templatename "$templateName" --description "$desc"
 
-  echo 'pausing for 10 seconds while the community creates it self...'
-  sleep 10
+  while [ "$communityCreated" = false ]; do
+    echo 'pausing for 10 seconds while the community creates it self...'
+    sleep 10
+
+    sfdx force:data:record:get -s Network -w "Name='$communityName'" > communityCreated.txt
+    if [ -s communityCreated.txt ]; then
+      communityCreated=true
+      rm communityCreated.txt
+      echo 'Community has been created...'
+    fi
+  done
 fi
 
 rm testingCommunityExists.txt
