cf69d746eb424bf16e549b1188e8cd512725ce83
Stewart Anderson <stewart.anderson@salesforce.com>
2022-11-14 14:14:00 +0000

    Added Multi-Key Functionality

--- qbrix/tools/utils/qbrix_nextgen_datatool.py
+++ qbrix/tools/utils/qbrix_nextgen_datatool.py
@@ -13,8 +13,8 @@ log = init_logger()
 
 class RunDataTool(BaseTask, ABC):
     task_options = {
-      "DataKey": {
-        "description": "Data Collection Key",
+      "data_keys": {
+        "description": "Data Collection Key or keys",
         "required": True
         },
       "total_timeout": {
@@ -26,83 +26,85 @@ class RunDataTool(BaseTask, ABC):
     def _init_options(self, kwargs):
       super(RunDataTool, self)._init_options(kwargs)
       self.url = "https://nxdo-data-tool.herokuapp.com/api/jobs/deployment"
-      self.data_key = self.options["DataKey"]
+      self.data_keys = self.options["data_keys"]
       self.total_timeout = int(self.options["total_timeout"]) if "total_timeout" in self.options else 1000
 
     def _run_task(self):
 
-      if self.data_key is None or self.data_key == "":
-        log.error("Invalid Data Collection ID passed. Please check the ID you have set for the Datakey option and try again.")
-        exit
+      for data_key in self.data_keys:
 
-      st = time.time()
+        if data_key is None or data_key == "":
+          log.debug(f"In valid Data Collection ID Passed. Skipping Data Collection ID: {data_key}")
+          continue
 
-      email_address = salesforce_query(f"SELECT Email From User Where Username = '{self.org_config.username}' LIMIT 1", self.org_config)
+        st = time.time()
 
-      if email_address is None:
-        exit
+        email_address = salesforce_query(f"SELECT Email From User Where Username = '{self.org_config.username}' LIMIT 1", self.org_config)
 
-      IsScratchOrg = True if isinstance(self.org_config, ScratchOrgConfig) else False
-
-      if self.org_config.is_sandbox:
-        IsScratchOrg = True
-        
-      headers = {"Content-Type": "application/json; charset=utf-8"}     
-      data = {
-        "username": self.org_config.username,
-        "email": email_address,
-        "collection_version_id": f"{self.data_key}",
-        "is_production": not IsScratchOrg
-      }
-
-      log.info(f"NextGen Data Tool: Starting Job\n\nRequesting Data Job with the following configuration:\n\nData Collection ID: {self.data_key}\nUsername: {self.org_config.username}\nEmail: {email_address}\nScratch Org Mode: {IsScratchOrg}\n")
-      result = requests.post(self.url, json=data, headers=headers)
-      jsonResponse = result.json()
-
-      if jsonResponse is not None:
-        job_id = jsonResponse["id"]
-        log.info(f"NextGen Data Tool: Job ID: {job_id} Started")
-      else:
-        log.error(f"NextGen Data Tool: Error Job failed to start.")
-        exit
-
-      
-      job_status_check_url = f"https://nxdo-data-tool.herokuapp.com/api/jobs/deployment/{job_id}"
-      
-      timeout = 0
-
-      if self.total_timeout < 500 or self.total_timeout > 8600:
-        self.total_timeout = 1000
-
-      while True:
-        check_job = requests.get(job_status_check_url)
-
-        if check_job.json() is None:
-          log.error("NextGen Data Tool: Unable to lookup job status. Check your internet connection.")
+        if email_address is None:
           exit
 
-        if timeout > self.total_timeout:
-          log.error("NextGen Data Tool: Error Data Load Timeout Reached")
+        IsScratchOrg = True if isinstance(self.org_config, ScratchOrgConfig) else False
+
+        if self.org_config.is_sandbox:
+          IsScratchOrg = True
+
+        headers = {"Content-Type": "application/json; charset=utf-8"}     
+        data = {
+          "username": self.org_config.username,
+          "email": email_address,
+          "collection_version_id": f"{data_key}",
+          "is_production": not IsScratchOrg
+        }
+
+        log.info(f"NextGen Data Tool: Starting Job\n\nRequesting Data Job with the following configuration:\n\nData Collection ID: {data_key}\nUsername: {self.org_config.username}\nEmail: {email_address}\nScratch Org Mode: {IsScratchOrg}\n")
+        result = requests.post(self.url, json=data, headers=headers)
+        jsonResponse = result.json()
+
+        if jsonResponse is not None:
+          job_id = jsonResponse["id"]
+          log.info(f"NextGen Data Tool: Job ID: {job_id} Started")
+        else:
+          log.error(f"NextGen Data Tool: Error Job failed to start.")
           exit
 
-        check_job_json = check_job.json()
-        status = check_job_json["state"]
-        progress = check_job_json["progress"]
-
-        if status == "completed":
-          break
-
-        if status == "active":
-          log.info(f"NextGen Data Tool: Job ID {job_id} still deploying...")
         
-        if status != "active" and status != "completed":
-          log.debug(f"NextGen Data Tool: Unsupported status ({status}) read. Stopping deployment")
-          exit
-
-        sleep(2)
-        timeout += 1
-
+        job_status_check_url = f"https://nxdo-data-tool.herokuapp.com/api/jobs/deployment/{job_id}"
         
-      et = time.time()
-      elapsed_time = et - st
-      log.info(f"Job Complete! Total Time: " + time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+        timeout = 0
+
+        if self.total_timeout < 500 or self.total_timeout > 8600:
+          self.total_timeout = 1000
+
+        while True:
+          check_job = requests.get(job_status_check_url)
+
+          if check_job.json() is None:
+            log.error("NextGen Data Tool: Unable to lookup job status. Check your internet connection.")
+            exit
+
+          if timeout > self.total_timeout:
+            log.error("NextGen Data Tool: Error Data Load Timeout Reached")
+            exit
+
+          check_job_json = check_job.json()
+          status = check_job_json["state"]
+          progress = check_job_json["progress"]
+
+          if status == "completed":
+            break
+
+          if status == "active":
+            log.info(f"NextGen Data Tool: Job ID {job_id} still deploying...")
+          
+          if status != "active" and status != "completed":
+            log.debug(f"NextGen Data Tool: Unsupported status ({status}) read. Stopping deployment")
+            exit
+
+          sleep(2)
+          timeout += 1
+
+          
+        et = time.time()
+        elapsed_time = et - st
+        log.info(f"Job Complete! Total Time: " + time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
