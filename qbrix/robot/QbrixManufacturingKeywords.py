afe13d2db4ffef309d82cf54128cfb93ce351197
Jesse Chase <jchase@salesforce.com>
2023-02-20 15:33:24 -0700

    Update QbrixManufacturingKeywords.py

--- qbrix/robot/QbrixManufacturingKeywords.py
+++ qbrix/robot/QbrixManufacturingKeywords.py
@@ -75,3 +75,15 @@ class QbrixManufacturingKeywords(BaseLibrary):
         if visible:
             self.browser.click(enable_mfgservice_toggle)
             sleep(2)
+
+    def enable_automotive_cloud_setting(self):
+        """
+        Enables Service Console for Manufacturing Setting in Salesforce Setup
+        """
+        enable_mfgservice_toggle = "span.slds-checkbox_off"
+        self.shared.go_to_setup_admin_page("MfgServiceExcellenceSettings/home")
+        self.browser.wait_for_elements_state("h2:text-is('Service Console for Manufacturing')", ElementState.visible, '30s')
+        visible = "visible" in self.browser.get_element_states(enable_mfgservice_toggle)
+        if visible:
+            self.browser.click(enable_mfgservice_toggle)
+            sleep(2)
