759bd61bb82a068582eb0955f52d4990ab884f16
Stewart Anderson <stewart.anderson@salesforce.com>
2023-03-14 12:45:23 +0000

    Added OrgHydrate to qbrix_preflight

--- qbrix/tools/utils/qbrix_preflight.py
+++ qbrix/tools/utils/qbrix_preflight.py
@@ -1,11 +1,13 @@
+import json
 import os
 from abc import ABC
-import subprocess
-import time
 
+import requests
+from cumulusci.core.exceptions import CommandException
+from cumulusci.core.keychain import BaseProjectKeychain
 from cumulusci.core.tasks import BaseTask
 from cumulusci.core.config import ScratchOrgConfig
-from qbrix.tools.shared.qbrix_console_utils import init_logger, run_command
+from qbrix.tools.shared.qbrix_console_utils import init_logger
 from qbrix.salesforce.qbrix_salesforce_tasks import QbrixInstallCheck
 from qbrix.tools.shared.qbrix_cci_tasks import run_cci_task, run_cci_flow
 
@@ -14,18 +16,10 @@ log = init_logger()
 
 class RunPreflight(BaseTask, ABC):
     task_docs = """
-    Runs pre-flight checks and deployments for target Salesforce orgs.
-
-    Supports information only mode where no tasks are executed. Use the command cci task run qbrix_preflight --info_mode True --org OrgAliasHere
-
-    Support Base Config and Base Data Deployments. Set the include_base_config, base_config_only_scratch and only_base_config options accordingly
+    Q Brix Preflight runs multiple tasks and flows against your target org to prepare it for the main deployment. By default it deploys settings and ensures that the Q Brix Registration package is installed.
     """
 
     task_options = {
-        "info_mode": {
-            "description": "Set to True if you just want to see what tasks this will run, without running them.",
-            "required": False
-        },
         "include_base_config": {
             "description": "Set to True if you want the base config and data deployed into the target org. Defaults to False",
             "required": False
@@ -38,29 +32,183 @@ class RunPreflight(BaseTask, ABC):
             "description": "Set to True if you only want to deploy base config and NOT base data. Defaults to False",
             "required": False
         },
+        "skip_settings_deployment": {
+            "description": "Set to True if you don't want preflight to deploy settings. Defaults to False",
+            "required": False
+        },
+        "skip_hydrate": {
+            "description": "Set to True if you don't want preflight to hydrate options for where clauses. Defaults to False",
+            "required": False
+        },
         "org": {
             "description": "org alias",
             "required": False
         },
-        "source_dependencies": {
-            "description": "[BETA] ONLY USE IN LEVEL 5 Q BRIX - Add a list of GitHub Q Brix Repo Locations which you want to check and install pre-deployment",
+        "accesstoken": {
+            "description": "Access Token for the target org, if known.",
+            "required": False
+        },
+        "instanceurl": {
+            "description": "Access Token for the target org, if known.",
             "required": False
         }
     }
 
     salesforce_task = True
+    keychain_class = BaseProjectKeychain
 
     def _init_options(self, kwargs):
         super(RunPreflight, self)._init_options(kwargs)
-        self.scratch_org_mode = False
-        self.include_base_config = self.options[
-            "include_base_config"] if "include_base_config" in self.options else False
-        self.base_config_only_scratch = self.options[
-            "base_config_only_scratch"] if "base_config_only_scratch" in self.options else False
+
+        # Initiate Shared Variables
+        self.scratch_org_mode = True if isinstance(self.org_config, ScratchOrgConfig) else False
+        self.targetusername = ""
+        self.accesstoken = self.options["accesstoken"] if "accesstoken" in self.options else self.org_config.access_token
+        self.instanceurl = self.options["instanceurl"] if "instanceurl" in self.options else self.org_config.instance_url
+
+        # Initiate Options
+        self.include_base_config = self.options["include_base_config"] if "include_base_config" in self.options else False
+        self.base_config_only_scratch = self.options["base_config_only_scratch"] if "base_config_only_scratch" in self.options else False
         self.info_mode = self.options["info_mode"] if "info_mode" in self.options else False
         self.only_base_config = self.options["only_base_config"] if "only_base_config" in self.options else False
-        self.source_dependencies = self.options[
-            "source_dependencies"] if "source_dependencies" in self.options else None
+        self.skip_settings_deployment = self.options["skip_settings_deployment"] if "skip_settings_deployment" in self.options else False
+        self.skip_hydrate = self.options["skip_hydrate"] if "skip_hydrate" in self.options else False
+
+    def deploy_settings(self):
+        # Deploy Settings if Present
+        if os.path.exists("force-app/main/default/settings") and not self.skip_settings_deployment:
+            log.info("PREFLIGHT: Settings directory found. Starting Deployment...")
+            settings_deploy_result = run_cci_task("deploy_settings", self.org_config.name, path="force-app/main/default/settings")
+            if settings_deploy_result:
+                log.info("PREFLIGHT: Settings Deployment Complete!")
+            else:
+                log.error("PREFLIGHT: Settings Deployment Failed. Check errors and warnings (if any) mentioned above.")
+        else:
+            log.info("PREFLIGHT: Skipping Settings Deployment.")
+
+    def deploy_qbrix_register(self):
+        log.info(f"PREFLIGHT: Deploying Q Brix Registration to Org {self.org_config.name}")
+        checkreg_deploy_result = run_cci_task("base:check_register", self.org_config.name)
+        if checkreg_deploy_result:
+            log.info("PREFLIGHT: Register Check Complete!")
+        else:
+            log.error("PREFLIGHT: Register Check Failed. Check errors and warnings (if any) mentioned above.")
+
+    def deploy_base_config_and_data(self):
+        log.info("Checking and loading Q Brix Base Config and Data")
+
+        if not QbrixInstallCheck("QBrix-0-xDO-BaseConfig", self.org_config):
+            log.info("Installing Q Brix Base Config")
+            deploy_result = run_cci_flow(f"base:deploy_qbrix", self.org_config.name)
+
+            if deploy_result:
+                log.info(f"Q Brix Base Config Deployment Complete!")
+            else:
+                log.error(f"Q Brix Base Config Deployment Failed. Check errors and warnings (if any) mentioned above.")
+        else:
+            log.info("Q Brix Base Config Deployed")
+
+        if not self.only_base_config:
+            if not QbrixInstallCheck("QBrix-0-xDO-BaseData", self.org_config):
+                log.info("Installing Q Brix Base Data")
+                deploy_result = run_cci_flow(f"base:deploy_qbrix_base_data", self.org_config.name)
+
+                if deploy_result:
+                    log.info(f"Q Brix Base Data Deployment Complete!")
+                else:
+                    log.error(f"Q Brix Base Data Deployment Failed. Check errors and warnings (if any) mentioned above.")
+            else:
+                log.info("Q Brix Base Data Deployed")
+
+    def _get_org_max_api_version(self):
+        url = f"{self.instanceurl}/services/data/"
+        headers = {
+            'Authorization': f'Bearer {self.accesstoken}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("GET", url, headers=headers)
+        data = json.loads(response.text)
+        #self.logger.info(response.text)
+        return float(data[-1]['version'])
+
+    def _is_qbrix_installed(self, qbrixname):
+        url = f"{self.instanceurl}/services/data/v56.0/query/?q=select+MasterLabel+from+xDO_Base_QBrix_Register__mdt+where+MasterLabel='{qbrixname}'"
+        headers = {
+            'Authorization': f'Bearer {self.accesstoken}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("GET", url, headers=headers)
+        data = json.loads(response.text)
+        self.logger.info(data["totalSize"])
+        return data["totalSize"] == 1
+
+    def _is_package_namespace_installed(self, namespace):
+        url = f"{self.instanceurl}/services/data/v56.0/query/?q=select+NamespacePrefix+from+PackageLicense+where+NamespacePrefix='{namespace}'"
+        headers = {
+            'Authorization': f'Bearer {self.accesstoken}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("GET", url, headers=headers)
+        data = json.loads(response.text)
+        self.logger.info(data["totalSize"])
+        return data["totalSize"] == 1
+
+    def _is_object_present_in_org(self, targetobject):
+        # SELECT  QualifiedApiName FROM EntityDefinition Where QualifiedApiName=
+
+        url = f"{self.instanceurl}/services/data/v56.0/query/?q=select+QualifiedApiName+from+EntityDefinition+where+QualifiedApiName='{targetobject}' LIMIT 1"
+        headers = {
+            'Authorization': f'Bearer {self.accesstoken}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("GET", url, headers=headers)
+        data = json.loads(response.text)
+        self.logger.info(data["totalSize"])
+        return data["totalSize"] == 1
+
+    def _is_psl_present_in_org(self, psl):
+        # e.g.
+        # SELECT  id,MasterLabel,DeveloperName from PermissionSetLicense where (Masterlabel='OmniStudioDesigner' or DeveloperName='OmniStudioDesigner')
+
+        url = f"{self.instanceurl}/services/data/v56.0/query/?q=select+Id+from+PermissionSetLicense+where(Masterlabel='{psl}' or DeveloperName='{psl}') LIMIT 1"
+        headers = {
+            'Authorization': f'Bearer {self.accesstoken}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("GET", url, headers=headers)
+        data = json.loads(response.text)
+        self.logger.info(data["totalSize"])
+        return data["totalSize"] == 1
+
+    def _prepruntime(self):
+
+        if self.scratch_org_mode:
+            self.targetusername = self.org_config.username
+        else:
+            self.targetusername = self.org_config.access_token
+
+        if not self.org_config.max_org_api_version:
+            self.org_config.max_org_api_version = self._get_org_max_api_version()
+
+        if not self.org_config.is_qbrix_installed:
+            self.org_config.is_qbrix_installed = self._is_qbrix_installed
+
+        if not self.org_config.is_object_in_org:
+            self.org_config.is_object_in_org = self._is_object_present_in_org
+
+        if not self.org_config.is_psl_in_org:
+            self.org_config.is_psl_in_org = self._is_psl_present_in_org
+
+        if not self.org_config.is_namespace_installed:
+            self.org_config.is_namespace_installed = self._is_package_namespace_installed
+
+    def _handle_returncode(self, returncode, stderr):
+        if returncode:
+            message = "Return code: {}".format(returncode)
+            if stderr:
+                message += "\nstderr: {}".format(stderr.read().decode("utf-8"))
+            self.logger.error(message)
+            raise CommandException(message)
 
     def scratch_org_tasks(self):
         if self.include_base_config:
@@ -71,147 +219,26 @@ class RunPreflight(BaseTask, ABC):
             self.deploy_base_config_and_data()
 
     def shared_tasks(self):
+        # Deploy Settings
+        if not self.skip_settings_deployment:
+            self.deploy_settings()
 
-        # Deploy Settings if Present
-        log.info(f"Check and Deploy Settings to Org {self.org_config.name}")
-        if os.path.exists("force-app/main/default/settings"):
-            log.info("Settings directory found. Deploying.")
-            if not self.info_mode:
-
-                settings_deploy_result = run_cci_task("deploy_settings", self.org_config.name, path="force-app/main/default/settings")
-
-                if settings_deploy_result:
-                    log.info("Settings Deployment Complete!")
-                else:
-                    log.error("Settings Deployment Failed. Check errors and warnings (if any) mentioned above.")
-        else:
-            log.info("No Settings Directory Found, skipping")
-
-        # Check and Deploy Q Brix Register
-        if not self.info_mode:
-            log.info(f"Deploying Q Brix Registration to Org {self.org_config.name}")
-
-            checkreg_deploy_result = run_cci_task("base:check_register", self.org_config.name)
-
-            if checkreg_deploy_result:
-                log.info("Register Check Complete!")
-            else:
-                log.error("Register Check Failed. Check errors and warnings (if any) mentioned above.")
-
-        else:
-            log.info(
-                f"[INFO ONLY] Org with alias {self.org_config.name} would have the Q Brix Package installed if it is not already installed")
-
-        if self.source_dependencies:
-            log.error("THIS FUNCTION IS NOT SUPPORTED AT THIS TIME. PLEASE INSTALL SOURCES USING THE SORUCE_DEPENCENCIES FLOW")
-
-        #     log.info("Checking Source Dependencies...")
-
-        #     # Check which Q Brix need to be installed
-
-        #     qbrix_install_dict = {}
-
-        #     for source in list(self.source_dependencies):
-
-        #         # Clear Invalid Sources
-        #         if "/" not in source:
-        #             log.error(f"{source} is not in a valid format. Please provide the URL to the GitHub repo for the source.")
-        #             continue
-
-        #         # Exclude Known Q Brix which are not supported here
-        #         if source.rsplit('/', 1)[-1] in {"QBrix-0-xDO-BaseConfig", "QBrix-0-xDO-BaseData", "QBrix-1-xDO-Tool-QBrixRegister"}:
-        #             log.debug(f"{source} is one of the Q Brix installed by this function. Please use the correct options to deploy it.")
-        #             continue
-
-        #         # Get Key Information for Q Brix
-        #         for key, value_dict in self.project_config.sources.items():
-        #             if source.lower() == dict(value_dict).get("github").lower():
-        #                 qbrix_install_dict.update({key: dict(value_dict).get("github")})
-
-        #         # if source not in qbrix_install_dict.items():
-        #         #     log.error(f"The dependency you requested with name ({source.rsplit('/', 1)[-1]}) was not found in the project source list. Please add it to the source list or remove it from the task > source_dependencies option to remove this error. Moving onto the next dependency (if any)")
-        #         #     continue
-
-        #     # Install (if required) each Q Brix
-        #     for qbrix_source_name, qbrix_repo in qbrix_install_dict.items():
-
-        #         repo_qbrix_name = qbrix_repo.rsplit('/', 1)[-1]
-
-        #         log.info(f"Checking if {repo_qbrix_name} is already installed in Salesforce org with alias ({self.org_config.name})...")
-        #         if not QbrixInstallCheck(repo_qbrix_name, self.org_config):
-        #             # log.info(f"Installing {repo_qbrix_name}")
-
-        #             pass
-
-        #             # project_folder = os.getcwd()
-
-        #             # proc = subprocess.Popen(f"cd {project_folder} && cci flow run {qbrix_source_name}:deploy_qbrix --org {self.org_config.name}",
-        #             #         shell=True,
-        #             #         cwd=project_folder
-        #             #     )
-
-        #             # while proc.poll() is None:
-        #             #     print(f"{repo_qbrix_name} is still deploying...")
-        #             #     time.sleep(1)
-
-        #             # output, error = proc.communicate()
-
-        #             # print(output)
-
-        #             # if error:
-        #             #     log.error(error.decode("utf-8"))
-        #         else:
-        #             log.info(f"{repo_qbrix_name} is already installed in Salesforce org with alias ({self.org_config.name}). Moving onto the next dependency (if any).")
-        #             continue
-        # else:
-        #     log.info("No source dependencies defined for pre-install. Skipping Task")
-
-    def deploy_base_config_and_data(self):
-
-        log.info("Checking and loading Q Brix Base Config and Data")
-
-        if not QbrixInstallCheck("QBrix-0-xDO-BaseConfig", self.org_config):
-            if not self.info_mode:
-                log.info("Installing Q Brix Base Config")
-                deploy_result = run_cci_flow(f"base:deploy_qbrix", self.org_config.name)
-
-                if deploy_result:
-                    log.info(f"Q Brix Base Config Deployment Complete!")
-                else:
-                    log.error(f"Q Brix Base Config Deployment Failed. Check errors and warnings (if any) mentioned above.")
-            else:
-                log.info(
-                    f"[INFO ONLY] Org with alias {self.org_config.name} would have the Q Brix Base Config installed if it is not already installed")
-
-        if not self.only_base_config:
-            if not QbrixInstallCheck("QBrix-0-xDO-BaseData", self.org_config):
-                if not self.info_mode:
-                    log.info("Installing Q Brix Base Data")
-                    deploy_result = run_cci_flow(f"base:deploy_qbrix_base_data", self.org_config.name)
-
-                    if deploy_result:
-                        log.info(f"Q Brix Base Data Deployment Complete!")
-                    else:
-                        log.error(f"Q Brix Base Data Deployment Failed. Check errors and warnings (if any) mentioned above.")
-                else:
-                    log.info(
-                        f"[INFO ONLY] Org with alias {self.org_config.name} would have the Q Brix Base Data installed if it is not already installed")
+        # Check and deploy Q Brix Register
+        self.deploy_qbrix_register()
 
     def _run_task(self):
+        log.info("PREFLIGHT: Starting Q Brix Preflight Check")
 
-        log.info("Starting QBrix Preflight Check")
-
-        if self.info_mode:
-            log.info("*** RUNNING AS INFORMATION MODE - NO TASKS WILL ACTUALLY BE RUN ***")
-
-        self.scratch_org_mode = True if isinstance(self.org_config, ScratchOrgConfig) else False
-        if self.scratch_org_mode:
-            log.info("Running in Scratch Org Mode")
-            self.scratch_org_tasks()
-        else:
-            log.info("Running in Production Org Mode")
-            self.production_org_tasks()
-
+        log.info("PREFLIGHT: Running Shared Tasks")
+        if not self.skip_hydrate:
+            self._prepruntime()
         self.shared_tasks()
 
-        log.info("Preflight Complete")
+        if self.scratch_org_mode:
+            log.info("PREFLIGHT: Running Scratch Org and Sandbox Related Tasks")
+            self.scratch_org_tasks()
+        else:
+            log.info("PREFLIGHT: Running Production Org Related Tasks")
+            self.production_org_tasks()
+
+        log.info("PREFLIGHT: Preflight Complete")
