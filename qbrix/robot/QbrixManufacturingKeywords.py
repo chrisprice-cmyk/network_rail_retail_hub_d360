6852df8e6f783702015e7f0b67ed3dce618f249a
Jesse Chase <jchase@salesforce.com>
2023-03-15 17:23:14 -0600

    Update QbrixManufacturingKeywords.py

--- qbrix/robot/QbrixManufacturingKeywords.py
+++ qbrix/robot/QbrixManufacturingKeywords.py
@@ -105,7 +105,7 @@ class QbrixManufacturingKeywords(BaseLibrary):
         Enables Partner Performance Management for Manufacturing Setting in Salesforce Setup
         """
         enable_toggle = "span.slds-checkbox_off"
-        self.shared.go_to_setup_admin_page("MfgPartnerVisitMgmtSettings/home")
+        self.shared.go_to_setup_admin_page("MfgPartnerPerfMgmtSettings/home")
         self.browser.wait_for_elements_state("h2:text-is('Partner Performance Management')", ElementState.visible, '30s')
         visible = "visible" in self.browser.get_element_states(enable_toggle)
         if visible:
