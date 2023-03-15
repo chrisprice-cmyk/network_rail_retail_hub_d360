4580847899c3c568a19e57e6e29eb07467adfeab
Jesse Chase <jchase@salesforce.com>
2023-03-15 17:36:17 -0600

    Update QbrixManufacturingKeywords.py

--- qbrix/robot/QbrixManufacturingKeywords.py
+++ qbrix/robot/QbrixManufacturingKeywords.py
@@ -106,7 +106,7 @@ class QbrixManufacturingKeywords(BaseLibrary):
         """
         enable_toggle = "span.slds-checkbox_off"
         self.shared.go_to_setup_admin_page("MfgPartnerPerfMgmtSettings/home")
-        self.browser.wait_for_elements_state("h2:text-is('Partner Performance Management')", ElementState.visible, '30s')
+        self.browser.wait_for_elements_state("p:text-is('Partner Performance Management')", ElementState.visible, '30s')
         visible = "visible" in self.browser.get_element_states(enable_toggle)
         if visible:
             self.browser.click(enable_toggle)
