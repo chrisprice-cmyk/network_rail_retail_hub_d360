e333a3b78d7e8e999b44c6d54ea2363315248207
Jesse Chase <jchase@salesforce.com>
2023-03-17 07:45:51 -0600

    Update QbrixNGOKeywords.py

--- qbrix/robot/QbrixNGOKeywords.py
+++ qbrix/robot/QbrixNGOKeywords.py
@@ -29,10 +29,18 @@ class QbrixNGOKeywords(BaseLibrary):
         """
         Enables Program and Benefit Management Settings for NGO Cloud
         """
-        enable_toggle = "span.slds-checkbox_off"
         self.shared.go_to_setup_admin_page("BenefitManagementSettings/home")
         self.browser.wait_for_elements_state("p:text-is('Program and Benefit Management')", ElementState.visible, '30s')
-        visible = "visible" in self.browser.get_element_states(enable_toggle)
-        if visible:
-            self.browser.click(enable_toggle)
-            sleep(2)
+        checked = "checked" in self.browser.get_element_states(
+            ":nth-match(label:has-text('Disabled'), 1)")
+        checked2 = "checked" in self.browser.get_element_states(
+            ":nth-match(label:has-text('Disabled'), 2)")
+        if not checked:
+            toggle_switch = self.browser.get_element(
+                ":nth-match(label:has-text('Disabled'), 1)")
+            self.browser.click(toggle_switch)
+            sleep(1)
+        if not checked2:
+            toggle_switch2 = self.browser.get_element(
+                ":nth-match(label:has-text('Disabled'), 2)")
+            self.browser.click(toggle_switch2)
