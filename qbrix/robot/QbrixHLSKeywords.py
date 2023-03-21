a4738e6bc23c7649bf7d33e3a8c045bfcf7592f5
Jesse Chase <jchase@salesforce.com>
2023-03-21 12:49:13 -0600

    Update QbrixHLSKeywords.py

--- qbrix/robot/QbrixHLSKeywords.py
+++ qbrix/robot/QbrixHLSKeywords.py
@@ -39,13 +39,23 @@ class QbrixHLSKeywords(BaseLibrary):
             self.browser.click(toggle_switch)
             sleep(1)
 
+        checked2 = "checked" in self.browser.get_element_states(
+            ":nth-match(label:has-text('Disabled'), 2)")
+        if not checked2:
+            toggle_switch = self.browser.get_element(
+                ":nth-match(label:has-text('Disabled'), 2)")
+            self.browser.click(toggle_switch)
+            sleep(2)
+            if "visible" in self.browser.get_element_states("button:has-text('Enable')"):
+                QbrixSharedKeywords().click_button_with_text("Enable")
+                sleep(5)
+
     def enable_care_plans_grantmaking(self):
         """
         Enables Care Plans Grantmaking for HLS
         """
-
         self.shared.go_to_setup_admin_page("CarePlanSettings/home")
-        self.browser.wait_for_elements_state("p:text-is('Grantmaking')", ElementState.visible, '30s')
+        self.browser.wait_for_elements_state("p:text-is('Care Plans')", ElementState.visible, '30s')
         checked = "checked" in self.browser.get_element_states(
             ":nth-match(label:has-text('Disabled'), 1)")
         if not checked:
@@ -53,6 +63,7 @@ class QbrixHLSKeywords(BaseLibrary):
                 ":nth-match(label:has-text('Disabled'), 1)")
             self.browser.click(toggle_switch)
             sleep(1)
+
         checked2 = "checked" in self.browser.get_element_states(
             ":nth-match(label:has-text('Disabled'), 2)")
         if not checked2:
