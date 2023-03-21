a7a13a3f9ea1e61c418c21fbde16ffe24cc9dd35
Jesse Chase <jchase@salesforce.com>
2023-03-21 10:23:47 -0600

    Update QbrixHLSKeywords.py

--- qbrix/robot/QbrixHLSKeywords.py
+++ qbrix/robot/QbrixHLSKeywords.py
@@ -44,20 +44,14 @@ class QbrixHLSKeywords(BaseLibrary):
         Enables Care Plans Grantmaking for HLS
         """
         self.shared.go_to_setup_admin_page("CarePlanSettings/home")
-        self.browser.wait_for_elements_state("p:text-is('Care Plans')", ElementState.visible, '30s')
-
-        checked = "checked" in self.browser.get_element_states(
-            ":nth-match(label:has-text('Disabled'), 1)")
-        if not checked:
-            toggle_switch = self.browser.get_element(
-                ":nth-match(label:has-text('Disabled'), 1)")
-            self.browser.click(toggle_switch)
-            sleep(5)
-
+        self.browser.wait_for_elements_state("p:text-is('Grantmaking')", ElementState.visible, '30s')
         checked2 = "checked" in self.browser.get_element_states(
             ":nth-match(label:has-text('Disabled'), 2)")
         if not checked2:
             toggle_switch = self.browser.get_element(
                 ":nth-match(label:has-text('Disabled'), 2)")
             self.browser.click(toggle_switch)
-            sleep(1)
+            sleep(2)
+            if "visible" in self.browser.get_element_states("button:has-text('Enable')"):
+                QbrixSharedKeywords().click_button_with_text("Enable")
+                sleep(5)
