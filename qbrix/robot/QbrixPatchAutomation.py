7714515adf8ad5b2bc2574be9959e4d9201924c5
Stewart Anderson <stewart.anderson@salesforce.com>
2023-07-18 14:33:50 +0100

    Updates
    - Disable Flow keyword added

--- qbrix/robot/QbrixPatchAutomation.py
+++ qbrix/robot/QbrixPatchAutomation.py
@@ -26,8 +26,19 @@ class QbrixPatchAutomation(QbrixRobotTask):
     def disable_field_service(self):
         self.field_service_keywords.enable_field_service(turn_off=True)  
 
-    def enable_messaging_channels(self):
-        pass
+    def deactivate_flow(self, flow_name: str):
+        if not flow_name:
+            return
+        
+        toolingapi = self.cumulusci._init_api("tooling/")
+        result = toolingapi.query(f"SELECT Id from Flow where MasterLabel = '{flow_name}' AND Status = 'Active'")
+        
+        if result['totalSize'] > 0:
+            self.browser.go_to(f"{self.cumulusci.org.instance_url}/builder_platform_interaction/flowBuilder.app?flowId={result['records'][0]['Id']}")
+            sleep(3)
+            self.browser.click("button:text-is('Deactivate')")
+            sleep(2)
+        
 
     def update_sdo_service_omniflow(self):
         pass 
