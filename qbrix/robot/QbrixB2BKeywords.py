4bdf33b78212e33b248b89fa91286bcb32dd6d49
Stewart Anderson <stewart.anderson@salesforce.com>
2023-10-16 15:43:26 +0100

    Added Logging for CPQ and Billing

--- qbrix/robot/QbrixB2BKeywords.py
+++ qbrix/robot/QbrixB2BKeywords.py
@@ -27,8 +27,15 @@ class QbrixB2BKeywords(QbrixRobotTask):
         )
 
         if results["totalSize"] == 1:
-            return results["records"][0]["Id"]
+            result_id = results["records"][0]["Id"]
+            self.builtin.log_to_console(
+                f"\n -> Found Id [{result_id}] for the Web Store [{store_name}]."
+            )
+            return result_id
 
+        self.builtin.log_to_console(
+            f"\n -> No Id for the Web Store Name provided [{store_name}] was found."
+        )
         return None
 
     def start_reindex(self, store_name: str = None):
@@ -42,7 +49,9 @@ class QbrixB2BKeywords(QbrixRobotTask):
         # Go To Index Page
         store_id = self.get_store_id(store_name)
         if store_id:
-            self.builtin.log_to_console(f"\nFound Store ID: {store_id}")
+            self.builtin.log_to_console(
+                f"\n -> Starting reindex of Store ID: {store_id}"
+            )
             self.browser.go_to(
                 f"{self.cumulusci.org.instance_url}/lightning/page/commerceSearch?lightning__webStoreId={store_id}&ws=%2Flightning%2Fr%2FWebStore%2F{store_id}%2Fview"
             )
@@ -57,9 +66,8 @@ class QbrixB2BKeywords(QbrixRobotTask):
                 selector=":nth-match(button.slds-button:text-is('Rebuild'):visible, 1)",
                 post_click_sleep=3,
             )
-        else:
             self.builtin.log_to_console(
-                "\nNo Store ID Found. Ensure that B2B Commerce is enabled within the Org"
+                "\n -> Reindex requested! It may take a few minutes for products to appear in the experience cloud site, depending on how many products you have."
             )
 
     def enable_b2b2c_for_sdo(self, store_name):
@@ -69,6 +77,10 @@ class QbrixB2BKeywords(QbrixRobotTask):
         Args:
             store_name (str): The name of the store.
         """
+
+        self.builtin.log_to_console(
+            f"\nRunning automation to check and enable demo integrations for store called [{store_name}]..."
+        )
 
         integration_button_selector = (
             ":nth-match(button.slds-button:text-is('Link Integration'):visible, 1)"
@@ -86,6 +98,7 @@ class QbrixB2BKeywords(QbrixRobotTask):
         store_id = self.get_store_id(store_name)
         if store_id:
             # Go To Tax Page and enable Tax Integration
+            self.builtin.log_to_console("\n -> Checking and enabling Tax Integration")
             self.browser.go_to(
                 f"{self.cumulusci.org.instance_url}/lightning/page/storeDetail?lightning__webStoreId={store_id}&ws=%2Flightning%2Fr%2FWebStore%2F{store_id}%2Fview&storeDetail__selectedTab=store_tax"
             )
@@ -101,8 +114,12 @@ class QbrixB2BKeywords(QbrixRobotTask):
                     selector=next_button_selector, post_click_sleep=2
                 )
                 self.shared.wait_and_click(confirm_button_selector)
+            self.builtin.log_to_console("\n -> Tax Integration Complete!")
 
             # Go To Shipping Calculation Page and Apply Integration
+            self.builtin.log_to_console(
+                "\n -> Checking and enabling Shipping Calculation Integration"
+            )
             self.browser.go_to(
                 f"{self.cumulusci.org.instance_url}/lightning/page/storeDetail?lightning__webStoreId={store_id}&ws=%2Flightning%2Fr%2FWebStore%2F{store_id}%2Fview&storeDetail__selectedTab=store_shipping"
             )
@@ -113,8 +130,14 @@ class QbrixB2BKeywords(QbrixRobotTask):
                 self.shared.wait_and_click(
                     selector=integration_button_selector, post_click_sleep=2
                 )
+            self.builtin.log_to_console(
+                "\n -> Shipping Calculation Integration Complete!"
+            )
 
             # Go To Card Payment Gateway Page and Apply Integration
+            self.builtin.log_to_console(
+                "\n -> Checking and enabling Card Payments Gateway Integration"
+            )
             self.browser.go_to(
                 f"{self.cumulusci.org.instance_url}/lightning/page/storeDetail?lightning__webStoreId={store_id}&ws=%2Flightning%2Fr%2FWebStore%2F{store_id}%2Fview&storeDetail__selectedTab=store_payment"
             )
@@ -125,9 +148,17 @@ class QbrixB2BKeywords(QbrixRobotTask):
                 self.shared.wait_and_click(
                     selector=integration_button_selector, post_click_sleep=2
                 )
+            self.builtin.log_to_console(
+                "\n -> Card Payments Gateway Integration Complete!"
+            )
 
     def enable_salesforce_payments(self):
-        """Checks that payments have been enabled, if not already"""
+        """Checks that Salesforce Payments has been enabled in the Salesforce Org"""
+
+        self.builtin.log_to_console(
+            "\nRunning automation to check that Salesforce Payments has been enabled..."
+        )
+
         self.shared.go_to_setup_admin_page("PaymentsSettings/home", 2)
         sleep(5)
         if "visible" in self.browser.get_element_states("button:text-is('Enable')"):
@@ -136,3 +167,8 @@ class QbrixB2BKeywords(QbrixRobotTask):
             )
             self.browser.click(select_button_locator)
             sleep(5)
+            self.builtin.log_to_console("\n -> Salesforce Payments is now enabled.")
+        else:
+            self.builtin.log_to_console(
+                "\n -> Salesforce Payments appears to already be enabled."
+            )
