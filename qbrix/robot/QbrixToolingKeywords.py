8e35d0d4b291fd5fb63e43363c070b7e7703dac4
rmasud-sf <rmasud@salesforce.com>
2023-10-25 15:03:04 -0500

    added waittime for connected app robot. some orgs are slower

--- qbrix/robot/QbrixToolingKeywords.py
+++ qbrix/robot/QbrixToolingKeywords.py
@@ -39,13 +39,14 @@ class QbrixToolingKeywords(QbrixRobotTask):
             self.browser.wait_for_elements_state(f"{iframe_selector} h2.mainTitle:text-is('Connected App Detail')", ElementState.visible, "30s")
 
 
-    def enable_admin_auth_for_connected_app(self, connected_app_name, browse_to_app_page: bool = True):
+    def enable_admin_auth_for_connected_app(self, connected_app_name, browse_to_app_page: bool = True, waittime:str="15s"):
         """
         Updates the User Policy for a connected up to 'Admin approved users are pre-authorized'
 
         Args:
             connected_app_name (str): The label for the Connected App
             browse_to_app_page (bool): Set to true (The Default) when you want this function to browse to the connected app page. This can be set to False if it is assumed the browser will be on the connected app page, when its run.
+            waittime override for orgs that are a little slower
         """
         try:
             if browse_to_app_page:
@@ -54,35 +55,36 @@ class QbrixToolingKeywords(QbrixRobotTask):
             iframe_selector = self.shared.iframe_handler()
             if self.browser.get_element_count(f"{iframe_selector} td.dataCol:text-is('Admin approved users are pre-authorized')") == 0:
                 self.browser.click(f"{iframe_selector} .btn:has-text('Edit Policies')")
-                self.browser.wait_for_elements_state(f"{iframe_selector} h2.mainTitle:text-is('Connected App Edit')", ElementState.visible, "15s")
+                self.browser.wait_for_elements_state(f"{iframe_selector} h2.mainTitle:text-is('Connected App Edit')", ElementState.visible, waittime)
                 self.browser.select_options_by(f"{iframe_selector} #userpolicy", SelectAttribute.text, "Admin approved users are pre-authorized")
                 sleep(4)
                 if "visible" in self.browser.get_element_states("iframe >>> .btn:has-text('Ok')"):
                     self.browser.click("iframe >>> .btn:has-text('Ok')")
                     sleep(2)
-                self.browser.wait_for_elements_state(f"{iframe_selector} .btn:has-text('Save')", ElementState.visible, "15s")
+                self.browser.wait_for_elements_state(f"{iframe_selector} .btn:has-text('Save')", ElementState.visible, waittime)
                 self.browser.click(f"{iframe_selector} .btn:has-text('Save')")
-                self.browser.wait_for_elements_state(f"{iframe_selector} h2.mainTitle:text-is('Connected App Detail')", ElementState.visible, "15s")
+                self.browser.wait_for_elements_state(f"{iframe_selector} h2.mainTitle:text-is('Connected App Detail')", ElementState.visible, waittime)
         except Exception as e:
             self.browser.take_screenshot()
             raise e
 
-    def enable_system_admin_profile_for_connected_app(self, connected_app_name: str, browse_to_app_page: bool = True):
+    def enable_system_admin_profile_for_connected_app(self, connected_app_name: str, browse_to_app_page: bool = True, waittime:str="15s"):
         """
         Enables the System Administrator Profile for a connected app
 
         Args:
             connected_app_name (str): The label for the Connected App
             browse_to_app_page (bool): Set to true (The Default) when you want this function to browse to the connected app page. This can be set to False if it is assumed the browser will be on the connected app page, when its run.
+            waittime override for orgs that are a little slower
         """
         try:
             if browse_to_app_page:
                 self.go_to_connected_app_page(connected_app_name)
 
             iframe_selector = self.shared.iframe_handler()
-            self.browser.wait_for_elements_state(f"{iframe_selector} .btn:has-text('Manage Profiles')", ElementState.visible, "15s")
+            self.browser.wait_for_elements_state(f"{iframe_selector} .btn:has-text('Manage Profiles')", ElementState.visible, waittime)
             self.browser.click(f"{iframe_selector} .btn:has-text('Manage Profiles')")
-            self.browser.wait_for_elements_state(f"{iframe_selector} h1:text-is('Application Profile Assignment')", ElementState.visible, "15s")
+            self.browser.wait_for_elements_state(f"{iframe_selector} h1:text-is('Application Profile Assignment')", ElementState.visible, waittime)
             if not "checked" in self.browser.get_element_states(f"{iframe_selector} tr:has-text('System Administrator') >> input"):
                 self.browser.click(f"{iframe_selector} tr:has-text('System Administrator') >> input")
                 self.browser.click(f"{iframe_selector} .btn:has-text('Save')")
@@ -95,8 +97,8 @@ class QbrixToolingKeywords(QbrixRobotTask):
     def enable_vbt_passport(self):
         """ Enables VBT (PostSpin) Connected App Settings"""
         connected_app_name = "VBT Connected"
-        self.enable_admin_auth_for_connected_app(connected_app_name)
-        self.enable_system_admin_profile_for_connected_app(connected_app_name, False)
+        self.enable_admin_auth_for_connected_app(connected_app_name,True,"40s")
+        self.enable_system_admin_profile_for_connected_app(connected_app_name, False,"40s")
         
     def enable_q_passport(self):
         """ Enables Q Passport Connected App Settings"""
