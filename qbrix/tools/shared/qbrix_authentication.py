1ae94f4f09802c3d13e774aff0925b25e60c61c1
Stewart Anderson <stewart.anderson@salesforce.com>
2023-11-08 15:51:02 +0000

    Updates for GLOOP
    - Fixed lookup parameter for needle cast
    - Added option to set auth token for gloop if already known

--- qbrix/tools/shared/qbrix_authentication.py
+++ qbrix/tools/shared/qbrix_authentication.py
@@ -1,13 +1,16 @@
 import base64
 import hashlib
 import hmac
+import json
 import struct
 import time
-import json
+
 import requests
+
 
 def qbrix_services_endpoint():
     return "https://qbrix-runtime-service-8c3413c48d7f.herokuapp.com"
+
 
 def generate_mfa_code(secret):
     # Convert the secret from base32 to bytes
@@ -22,35 +25,36 @@ def generate_mfa_code(secret):
 
     # Extract a 4-byte dynamic binary code from the HMAC result
     offset = hmac_result[-1] & 0x0F
-    dbc = struct.unpack(">L", hmac_result[offset:offset+4])[0] & 0x7FFFFFFF
+    dbc = struct.unpack(">L", hmac_result[offset : offset + 4])[0] & 0x7FFFFFFF
 
     # Convert the dynamic binary code to a 6-digit number
     mfa_code = dbc % 1000000
 
     return "{:06d}".format(mfa_code)
 
+
 def base32_to_bytes(base32_string):
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
     padding = len(base32_string) % 8
-    base32_string = base32_string + '=' * padding  # Add padding
+    base32_string = base32_string + "=" * padding  # Add padding
     result = []
 
     for i in range(0, len(base32_string), 8):
-        chunk = base32_string[i:i+8]
+        chunk = base32_string[i : i + 8]
         acc = 0
         for j, char in enumerate(chunk):
-            acc = acc | (alphabet.index(char.upper()) << (35 - 5*j))
+            acc = acc | (alphabet.index(char.upper()) << (35 - 5 * j))
 
-        result.extend(struct.pack(">Q", acc)[(8 - (5 * len(chunk) + 3) // 8):])
+        result.extend(struct.pack(">Q", acc)[(8 - (5 * len(chunk) + 3) // 8) :])
 
     return bytes(result)
 
-def get_secure_setting(secure_setting: str = None):
 
+def get_secure_setting(secure_setting: str = None):
     if not secure_setting:
         return None
 
-    endpoint=qbrix_services_endpoint()
+    endpoint = qbrix_services_endpoint()
     url = f"{endpoint}/QBrixQLabs?settingId={secure_setting}"
 
     try:
@@ -68,23 +72,21 @@ def get_secure_setting(secure_setting: str = None):
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return None
-    
+
+
 def get_who_am_i(accesstoken: str):
-    endpoint=qbrix_services_endpoint()
+    endpoint = qbrix_services_endpoint()
     url = f"{endpoint}/NeedleCast/whoami"
     payload = json.dumps({"accessToken": f"{accesstoken}"})
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    headers = {"Content-Type": "application/json"}
     response = requests.request("POST", url, headers=headers, data=payload)
     return response.json()
 
+
 def perform_needle_cast(username: str):
-    endpoint=qbrix_services_endpoint()
+    endpoint = qbrix_services_endpoint()
     url = f"{endpoint}/NeedleCast/authenticate"
-    payload = json.dumps({"username": f"{username}"})
-    headers = {
-    'Content-Type': 'application/json'
-    }
+    payload = json.dumps({"TargetUserId": f"{username}"})
+    headers = {"Content-Type": "application/json"}
     response = requests.request("POST", url, headers=headers, data=payload)
     return response.json()
