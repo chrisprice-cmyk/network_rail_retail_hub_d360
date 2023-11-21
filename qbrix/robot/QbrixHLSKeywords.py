2544222f06c93fe451c31d6b49236fb65ba984e3
Stewart Anderson <stewart.anderson@salesforce.com>
2023-11-21 14:12:58 +0000

    Update QbrixHLSKeywords.py

--- qbrix/robot/QbrixHLSKeywords.py
+++ qbrix/robot/QbrixHLSKeywords.py
@@ -11,6 +11,88 @@ class QbrixHLSKeywords(QbrixRobotTask):
 
     """Shared Keywords for HLS"""
 
+    def enable_care_gaps(self):
+        """
+        Enables Care Gaps for HLS
+        """
+        self.builtin.log_to_console("\n[HLS] Enabling Care Gaps...")
+        # Go to setup page
+        self.shared.go_to_setup_admin_page("IntegratedCareManagementSettings/home")
+        care_gap_selector = "h3.slds-card__header-title:has-text('Care Gap')"
+        self.browser.wait_for_elements_state(
+            care_gap_selector, ElementState.visible, "30s"
+        )
+        if not "checked" in self.browser.get_element_states(care_gap_selector):
+            self.browser.click(care_gap_selector)
+
+    def generate_decision_tables(self):
+        """
+        Generate Decision Tables for HLS
+        """
+        self.builtin.log_to_console("\n[HLS] Generating Decision Tables...")
+        self.shared.go_to_setup_admin_page("AdvancedTherapyManagementSettings/home")
+        # make sure iframe is considered
+        my_iframe_handler = self.shared.iframe_handler()
+        # expand that panel, only if it's not expanded already
+        if self.browser.get_element_count(
+            f"{my_iframe_handler} .slds-setup-assistant__item:has-text('Configure Decision Tables') button.slds-button_icon[aria-expanded='false']"
+        ):
+            self.builtin.log_to_console(
+                "\n[HLS] Found Generate Decision Tables not expanded"
+            )
+            self.browser.click(
+                f"{my_iframe_handler} .slds-setup-assistant__item:has-text('Configure Decision Tables') button.slds-button_icon[aria-expanded='false']"
+            )
+            if self.browser.get_element_count(
+                f"{my_iframe_handler} .slds-setup-assistant__item:has-text('Generate Decision Tables to Override Default Settings'):visible"
+            ):
+                self.builtin.log_to_console("\n[HLS] -> Expanded!")
+        # click the button whose text is Generate Decision Tables
+        self.shared.wait_and_click(
+            f"{my_iframe_handler} button.slds-button:text-is('Generate Decision Tables')"
+        )
+        # confirm to generate decision tables
+        if self.browser.get_element_count(
+            f"{my_iframe_handler} button.slds-button:text-is('Generate')"
+        ):
+            self.shared.wait_and_click(
+                f"{my_iframe_handler} button.slds-button:text-is('Generate')"
+            )
+            self.builtin.log_to_console("\n[HLS] Decision Tables Generated")
+        self.builtin.log_to_console("\n[HLS] Decision Tables Step Complete")
+
+    def enable_advanced_scheduling(self):
+        """
+        Enables Advanced Scheduling Settings for HLS
+        """
+        self.builtin.log_to_console("\n[HLS] Enabling Advanced Scheduling Settings...")
+        self.shared.go_to_setup_admin_page("AdvancedTherapyManagementSettings/home")
+        # make sure iframe is considered
+        my_iframe_handler = self.shared.iframe_handler()
+        # expand that panel, only if it's not expanded already
+        if self.browser.get_element_count(
+            f"{my_iframe_handler} .slds-setup-assistant__item:has-text('Activate Advanced Scheduling Settings') button.slds-button_icon[aria-expanded='false']"
+        ):
+            self.builtin.log_to_console(
+                "\n Found Activate Advanced Scheduled Settings not expanded"
+            )
+            self.browser.click(
+                f"{my_iframe_handler} .slds-setup-assistant__item:has-text('Activate Advanced Scheduling Settings') button.slds-button_icon[aria-expanded='false']"
+            )
+        # click the button whose text is Activate Advanced Scheduling
+        self.shared.wait_and_click(
+            f"{my_iframe_handler} button.slds-button:text-is('Activate Advanced Scheduling')"
+        )
+        # confirm to activate
+        if self.browser.get_element_count(
+            f"{my_iframe_handler} button.slds-button:text-is('Activate')"
+        ):
+            self.shared.wait_and_click(
+                f"{my_iframe_handler} button.slds-button:text-is('Activate')"
+            )
+            self.builtin.log_to_console("\n[HLS] -> Advanced Scheduling Enabled!")
+        self.builtin.log_to_console("\n[HLS] -> Advanced Scheduling Step Complete!")
+
     def enable_care_plans(self):
         """
         Enables Care Plans for HLS
