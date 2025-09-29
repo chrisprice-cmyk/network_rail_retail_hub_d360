ae5d0d815b6626b3f972a620fe6a8ab4a44eda7a
Radif Masud <rmasud@salesforce.com>
2025-09-29 16:52:46 -0500

    less is more to fix vnc (#124)

--- .devcontainer/container_poststart.sh
+++ .devcontainer/container_poststart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # for template changes - run this manually unless you enjoy watching your changes disappear.
 if [ \"${PWD##*/}\" != \"xDO-Template\" ]; then qx setup init-codespace; fi
-xrandr -s 1920x1080 
+printf "vscode\\nvscode\n\n" | vncpasswd && vncserver && xrandr -s 1920x1080 
 source activate
 #not yet
 #sf org login web -a QLABS -r https://qlabs-org.my.salesforce.com
